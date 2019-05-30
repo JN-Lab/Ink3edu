38155196ddbcbc12e3d1e93912af050d0487e6f9
juliennuellas <julien.nuellas@gmail.com>
2019-05-30 11:21:20 +0200

    Some modifications on trainings app: I intergate the urls in urls.by in settings (+ modification on apps.py in trainings app). I created a basic serializers-view-urls to validate the endpoint logic

--- ink3edu/trainings/apps.py
+++ ink3edu/trainings/apps.py
@@ -2,4 +2,10 @@ from django.apps import AppConfig
 
 
 class TrainingsConfig(AppConfig):
-    name = 'trainings'
+    name = 'ink3edu.trainings'
+
+    def ready(self):
+        try:
+            import ink3edu.trainings.signals
+        except ImportError:
+            pass
