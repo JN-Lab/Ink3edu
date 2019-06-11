a81399b76063db0b324ec2be597be11009c85bfa
juliennuellas <julien.nuellas@gmail.com>
2019-06-11 21:02:57 +0200

    started to write unit tests. But I have a problem regarding user model import (runtime error

--- ink3edu/trainings/tests/test_training_list.py
+++ ink3edu/trainings/tests/test_training_list.py
@@ -3,4 +3,26 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from ink3edu.helper_dbtestdata import TestDatabase
 from ink3edu.users.models import User
-from ..models import Training, Section, SectionsInTrainings, Chapter, ChaptersInSections, Status, Category
+from ink3edu.trainings.models import Training, Section, SectionsInTrainings, Chapter, ChaptersInSections, Status, Category
+
+class TrainingListTest(APITestCase):
+    """
+    This class will test all the interactions we can have with a list of Trainings.
+    What will be tested:
+        (PHASE 1 : no permissions)
+        -> Get all the trainings
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        """
+        We create a database to realize the different tests of the class
+        """
+        TestDatabase.create()
+
+    def test_get_all_trainings(self):
+        url = reverse('trainings_list')
+        response = self.client.get(url, format='json')
+        trainings = Training.objects.all()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(trainings)
