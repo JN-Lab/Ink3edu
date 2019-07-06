070c3e88cce8247236aa128480e09fbd1c870b50
juliennuellas <julien.nuellas@gmail.com>
2019-07-06 15:42:44 +0200

    I worked on Interactions App: regarding student serializer, the trainings attribute groups all the trainings associated to the student with the date of first_access, the active status and the done status

--- ink3edu/interactions/serializers.py
+++ ink3edu/interactions/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 
-from .models import Student, Mentor, MentorsBuildingTrainings
+from .models import Student, Mentor, MentorsBuildingTrainings, StudentsFollowingTrainings
+
+class StudentsFollowingTrainingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentsFollowingTrainings
+        fields = ('training', 'first_access', 'active', 'done')
 
 class StudentSerializer(serializers.ModelSerializer):
+    trainings = StudentsFollowingTrainingsSerializer(source='student_with_trainings', many=True, required=False)
+    
     class Meta:
         model = Student
         fields = ('user', 'trainings')
