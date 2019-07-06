fe8af19675bf15fc534c328e2232afb75ba89842
juliennuellas <julien.nuellas@gmail.com>
2019-07-06 15:49:24 +0200

    I worked on Interactions app: just addes some comments

--- ink3edu/interactions/serializers.py
+++ ink3edu/interactions/serializers.py
@@ -3,11 +3,18 @@ from rest_framework import serializers
 from .models import Student, Mentor, MentorsBuildingTrainings, StudentsFollowingTrainings
 
 class StudentsFollowingTrainingsSerializer(serializers.ModelSerializer):
+    """
+    This serializer just formats all the information of the trainings associated to a student
+    It is only used to build StudentSerializer
+    """
     class Meta:
         model = StudentsFollowingTrainings
         fields = ('training', 'first_access', 'active', 'done')
 
 class StudentSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used to have the list of students with the trainings they are following
+    """
     trainings = StudentsFollowingTrainingsSerializer(source='student_with_trainings', many=True, required=False)
     
     class Meta:
@@ -15,11 +22,18 @@ class StudentSerializer(serializers.ModelSerializer):
         fields = ('user', 'trainings')
 
 class MentorsBuildingTrainingsSerializer(serializers.ModelSerializer):
+    """
+    This serializer just formats all the information of the trainings the mentors are working on.
+    It is only used to build MentorSerializer
+    """
     class Meta:
         model = MentorsBuildingTrainings
         fields = ('training', 'role')
 
 class MentorSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used to have the list of mentors with the trainings they are building
+    """
     trainings = MentorsBuildingTrainingsSerializer(source='mentors_with_trainings', many=True, required=False)
 
     class Meta:
--- ink3edu/interactions/views.py
+++ ink3edu/interactions/views.py
@@ -4,9 +4,15 @@ from .models import Student, Mentor
 from .serializers import StudentSerializer, MentorSerializer
 
 class StudentList(generics.ListAPIView):
+    """
+    This view gets all the students information
+    """
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 class MentorList(generics.ListAPIView):
+    """
+    This view gets all the mentors information
+    """
     queryset = Mentor.objects.all()
     serializer_class = MentorSerializer
