167222725d29d0a64bcfdd3b59a067c174fa9fff
juliennuellas <julien.nuellas@gmail.com>
2019-07-07 12:53:47 +0200

    I worked on content app: First draw of models for MVP

---
+++ ink3edu/content/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils import timezone
+from ink3edu.interactions.models import Group
+from ink3edu.trainings.models import Chapter
+
+class ContentsInChapters:
+    """
+    This class represents the contents per chapters
+    This is an association table between Chapter and Content
+    where we organize the content inside the chapter
+    """
+    
+    chapter = models.ForeignKey('Chapter',
+                                on_delete=models.CASCADE,
+                                related_name='chapter_with_contents')
+    content = models.ForeignKey('Content',
+                                on_delete=models.CASCADE,
+                                related_name='content_per_chapters')
+    content_number = models.IntergerField()
+
+    def __str__(self):
+        return f"{self.chapter.title} - {self.content.title} - {self.content_number}"
+
+
+class Content(models.Model):
+    """
+    This class represents the contents created
+    """
+    
+    title = models.Charfield(max_length=250)
+    description = models.TextField()
+    group = models.ForeignKey(Group,
+                              on_delete=models.CASCADE,
+                              related_name='%(class)s_related')
+    chapters = models.ManyToManyField('Chapter',
+                                      through='ContentsInChapters',
+                                      related_name='contents')
+    url_content_readonly = models.URLField()
+    url_content_modification_allowed = models.URLField()
+
+    def __str__(self):
+        return self.title
